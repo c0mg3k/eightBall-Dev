7d8b0dd1c4a2f913604583242c1d326456ac45f3
c0mg3k <c0mg3k@outlook.com>
2017-04-27 00:41:07 -0500

    initial commit

---
+++ ngApp/app.ts
@@ -0,0 +1,36 @@
+namespace eightball_dev {
+
+    angular.module('eightball_dev', ['ui.router', 'ngResource', 'ui.bootstrap']).config((
+        $stateProvider: ng.ui.IStateProvider,
+        $urlRouterProvider: ng.ui.IUrlRouterProvider,
+        $locationProvider: ng.ILocationProvider
+    ) => {
+        // Define routes
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/ngApp/views/home.html',
+                controller: eightball_dev.Controllers.HomeController,
+                controllerAs: 'controller'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: '/ngApp/views/about.html',
+                controller: eightball_dev.Controllers.AboutController,
+                controllerAs: 'controller'
+            })
+            .state('notFound', {
+                url: '/notFound',
+                templateUrl: '/ngApp/views/notFound.html'
+            });
+
+        // Handle request for non-existent route
+        $urlRouterProvider.otherwise('/notFound');
+
+        // Enable HTML5 navigation
+        $locationProvider.html5Mode(true);
+    });
+
+    
+
+}
---
+++ ngApp/services/services.ts
@@ -0,0 +1,3 @@
+namespace eightball_dev.Services {
+
+    }
